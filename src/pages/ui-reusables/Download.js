3e657667443f535672bc8379f434d8dace2cef47
Yuvaraj S <yuvarajs@Yuvarajs-MacBook-Air.local>
2025-11-24 20:57:41 +0530

    full stack

--- src/pages/ui-reusables/Download.js
+++ src/pages/ui-reusables/Download.js
@@ -3,10 +3,10 @@ import React from "react";
 
 export default function Download() {
     const handleDownload = () => {
-        const resumeUrl = "/assets/YUVARAJ_FRONTEND_DEVELOPER.pdf"; // Path relative to public folder
+        const resumeUrl = "/assets/YUVARAJ_FULLSTACK_DEVELOPER.pdf"; // Path relative to public folder
         const link = document.createElement("a");
         link.href = resumeUrl;
-        link.download = "YUVARAJ_FRONTEND_DEVELOPER.pdf"; // Set custom file name for download
+        link.download = "YUVARAJ_FULLSTACK_DEVELOPER.pdf"; // Set custom file name for download
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
